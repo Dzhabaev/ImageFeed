10bb7c4f6acca5e39a25072547179d989b7a3a20
Chingiz <dzhabaev@outlook.com>
2023-10-23 16:35:22 +0300

    Подключена зависимость ProgressHUD

--- ImageFeed/Auth/SplashViewController.swift
+++ ImageFeed/Auth/SplashViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 final class SplashViewController: UIViewController {
     private let ShowAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
@@ -60,6 +61,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -72,8 +74,9 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
+                ProgressHUD.dismiss()
             case .failure:
-                // TODO [Sprint 11]
+                ProgressHUD.dismiss()
                 break
             }
         }
